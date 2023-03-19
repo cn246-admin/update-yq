62e3e4069492e709db3c291fae6eb00fec4d0f36
Chuck Nemeth <chuckn246@gmail.com>
2023-03-19 00:17:20 -0400

    Variable name consistency between gists

--- update-yq.sh
+++ update-yq.sh
@@ -7,9 +7,9 @@
 #######################
 # VARIABLES
 #######################
-bindir="$HOME/.local/bin"
-mandir="$HOME/.local/share/man/man1"
-tmpdir="$(mktemp -d /tmp/yq.XXXXXXXX)"
+bin_dir="$HOME/.local/bin"
+man_dir="$HOME/.local/share/man/man1"
+tmp_dir="$(mktemp -d /tmp/yq.XXXXXXXX)"
 
 if command -v yq >/dev/null; then
   yq_installed_version="$(yq --version | cut -d' ' -f 4)"
@@ -27,16 +27,16 @@ yq_man="yq.1"
 #######################
 # Define clean_up function
 clean_up () {
-  printf "Would you like to delete the tmpdir and the downloaded files? (Yy/Nn) "
+  printf '%s' "Would you like to delete the tmp_dir and the downloaded files? (Yy/Nn) "
   read -r choice
   case "${choice}" in
     [yY]|[yY]es)
       printf '%s\n' "Cleaning up install files"
-      cd && rm -rf "${tmpdir}"
+      cd && rm -rf "${tmp_dir}"
       exit "${1}"
       ;;
     *)
-      printf '%s\n' "Exiting without deleting files from ${tmpdir}"
+      printf '%s\n' "Exiting without deleting files from ${tmp_dir}"
       exit "${1}"
       ;;
   esac
@@ -91,10 +91,10 @@ esac
 # PATH CHECK
 #######################
 case :$PATH: in
-  *:"${bindir}":*)  ;;  # do nothing
+  *:"${bin_dir}":*)  ;;  # do nothing
   *)
-    code_red "[ERROR] ${bindir} was not found in \$PATH!"
-    code_red "[ERROR] Add ${bindir} to PATH or select another directory to install to"
+    code_red "[ERROR] ${bin_dir} was not found in \$PATH!"
+    code_red "Add ${bin_dir} to PATH or select another directory to install to"
     clean_up 1
     ;;
 esac
@@ -103,7 +103,7 @@ esac
 #######################
 # VERSION CHECK
 #######################
-cd "${tmpdir}" || exit
+cd "${tmp_dir}" || exit
 
 if [ "${yq_version}" = "${yq_installed_version}" ]; then
   code_yel "[WARN] Already using latest version. Exiting."
@@ -118,9 +118,9 @@ fi
 # DOWNLOAD
 #######################
 printf '%s\n' "Downloading yq archive and verification files"
-curl -sL -o "${tmpdir}/${yq_archive}.tar.gz" "${yq_url}/${yq_archive}.tar.gz"
-curl -sL -o "${tmpdir}/checksums" "${yq_url}/checksums"
-curl -sL -o "${tmpdir}/checksums_hashes_order" "${yq_url}/checksums_hashes_order"
+curl -sL -o "${tmp_dir}/${yq_archive}.tar.gz" "${yq_url}/${yq_archive}.tar.gz"
+curl -sL -o "${tmp_dir}/checksums" "${yq_url}/checksums"
+curl -sL -o "${tmp_dir}/checksums_hashes_order" "${yq_url}/checksums_hashes_order"
 
 printf '%s\n' "Extracting ${yq_archive}.tar.gz"
 tar -xf "${yq_archive}.tar.gz"
@@ -134,10 +134,10 @@ lineNumber=$(echo "$grepMatch" | cut -d: -f1)
 realLineNumber="$((lineNumber + 1))"
 
 awk -v ref="${yq_archive}.tar.gz" -v lin="$realLineNumber" \
-  'match($1, ref) { print $lin "  " $1}' checksums > "${tmpdir}/SHA512sums"
+  'match($1, ref) { print $lin "  " $1}' checksums > "${tmp_dir}/SHA512sums"
 
 printf '%s\n' "Verifying ${yq_archive}.tar.gz"
-if ! shasum -qc "${tmpdir}/SHA512sums"; then
+if ! shasum -qc "${tmp_dir}/SHA512sums"; then
   code_red "[ERROR] Problem with checksum!"
   clean_up 1
 fi
@@ -147,13 +147,13 @@ fi
 # PREPARE
 #######################
 # Create bin dir if it doesn't exist
-if [ ! -d "${bindir}" ]; then
-  mkdir -p "${bindir}"
+if [ ! -d "${bin_dir}" ]; then
+  mkdir -p "${bin_dir}"
 fi
 
 # Create man dir if it doesn't exist
-if [ ! -d "${mandir}" ]; then
-  mkdir -p "${mandir}"
+if [ ! -d "${man_dir}" ]; then
+  mkdir -p "${man_dir}"
 fi
 
 
@@ -161,15 +161,15 @@ fi
 # INSTALL
 #######################
 # Install yq binary
-if [ -f "${tmpdir}/${yq_archive}" ]; then
-  mv "${tmpdir}/${yq_archive}" "${bindir}/yq"
-  chmod 700 "${bindir}/yq"
+if [ -f "${tmp_dir}/${yq_archive}" ]; then
+  mv "${tmp_dir}/${yq_archive}" "${bin_dir}/yq"
+  chmod 700 "${bin_dir}/yq"
 fi
 
 # Install man page
-if [ -f "${tmpdir}/${yq_man}" ]; then
-  mv "${tmpdir}/${yq_man}" "${mandir}/${yq_man}"
-  chmod 640 "${mandir}/${yq_man}"
+if [ -f "${tmp_dir}/${yq_man}" ]; then
+  mv "${tmp_dir}/${yq_man}" "${man_dir}/${yq_man}"
+  chmod 640 "${man_dir}/${yq_man}"
 fi
 
 
